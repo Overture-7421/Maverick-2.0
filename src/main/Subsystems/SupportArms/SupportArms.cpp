1545d0b1ac1ad64909216dfad36589842aa7389a
GabrielReynaFranco <a01572655@tec.mx>
2024-08-14 12:56:57 -0600

    SupportArms Subsystem Start

--- src/main/Subsystems/SupportArms/SupportArms.cpp
+++ src/main/Subsystems/SupportArms/SupportArms.cpp
@@ -7,4 +7,23 @@
 SupportArms::SupportArms() = default;
 
 // This method will be called once per scheduler run
-void SupportArms::Periodic() {}
+void SupportArms::Periodic() { 
+
+    SetRightServoAngle(0.0); // Where the wanted positions of the servos are put on (ranges from 0.0 to 1.0)
+    SetLeftServoAngle(0.0);
+
+}
+
+void SupportArms::SetRightServoAngle(double angle) { //Conditional that allows us to invert the right servo if needed.
+    if (isRightServoInverted) {
+        angle = 1.0 - angle;
+    }
+    RightServo.Set(angle);
+}
+
+void SupportArms::SetLeftServoAngle(double angle) { //Conditional that allows us to invert the leftt servo if needed.
+    if (isLeftServoInverted) {
+        angle = 1.0 - angle;
+    }
+    LeftServo.Set(angle);
+}
--- src/main/Subsystems/SupportArms/SupportArms.h
+++ src/main/Subsystems/SupportArms/SupportArms.h
@@ -5,17 +5,23 @@
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
+#include <frc/Servo.h>
 
-class SupportArms : public frc2::SubsystemBase {
+
+class SupportArms : public frc2::SubsystemBase { //Class that extends to the SubsystemBase
  public:
-  SupportArms();
 
-  /**
-   * Will be called periodically whenever the CommandScheduler runs.
-   */
-  void Periodic() override;
+SupportArms(); //Constructor
+
+void Periodic() override; //Method that activates the susbystem
+
+void SetRightServoAngle(double angle); //Method to set the angle of the right servo
+void SetLeftServoAngle(double angle);  //Method to set the angle of the right servo
 
  private:
-  // Components (e.g. motor controllers and sensors) should generally be
-  // declared private and exposed only through public methods.
+  frc::Servo RightServo{0}; //Declaration of right servo and when will it be connected
+  frc::Servo LeftServo{1}; //Declaration of left servo and when will it be connected
+
+  bool isRightServoInverted = false; //Booleans that may invert one servo
+  bool isLeftServoInverted = true;
 };
