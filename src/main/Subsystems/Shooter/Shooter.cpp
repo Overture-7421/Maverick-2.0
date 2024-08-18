2a9a2c49eaf4c66f8a4a9a103735afaed1fa15a7
Eric Briseno <a01572515@tec.mx>
2024-08-18 17:26:57 -0600

    Starting Shooter Subsystem

--- src/main/Subsystems/Shooter/Shooter.cpp
+++ src/main/Subsystems/Shooter/Shooter.cpp
@@ -3,8 +3,19 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "Shooter.h"
+#include "Constants.h"
 
 Shooter::Shooter() = default;
+
+
+void Shooter::setVelocityVoltage(double velocity, double feedForward){
+    leftMotor.setFollow(25, true);
+    rightMotor.setVelocityVoltage(velocity, feedForward, false);
+}
+
+frc2::CommandPtr Shooter::shooterCommand(){
+    return this->RunOnce([this] {Shooter::setVelocityVoltage(Constants::ShooterSpeaker, 0);});
+};
 
 // This method will be called once per scheduler run
 void Shooter::Periodic() {}
--- src/main/Subsystems/Shooter/Shooter.h
+++ src/main/Subsystems/Shooter/Shooter.h
@@ -5,10 +5,16 @@
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
+#include "OvertureLib/MotorControllers/OverTalonFX/OverTalonFX.h"
+#include <frc2/command/Commands.h>
 
 class Shooter : public frc2::SubsystemBase {
  public:
   Shooter();
+
+  void setVelocityVoltage(double velocity, double feedForward);
+  frc2::CommandPtr shooterCommand();
+
 
   /**
    * Will be called periodically whenever the CommandScheduler runs.
@@ -16,6 +22,9 @@ class Shooter : public frc2::SubsystemBase {
   void Periodic() override;
 
  private:
+  OverTalonFX rightMotor{25, ControllerNeutralMode::Coast, false, "OverCANivore"};
+  OverTalonFX leftMotor{26, ControllerNeutralMode::Coast, false, "OverCANivore"};
+
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
 };
