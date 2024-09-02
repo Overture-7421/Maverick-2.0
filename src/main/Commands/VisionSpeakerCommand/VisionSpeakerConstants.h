f792601f4b10495c03d563681bcc15d21fa22b3d
Vanessa_Carranza <v.carranza.carri@gmail.com>
2024-09-02 17:25:41 -0600

    tabulate

--- src/main/Commands/VisionSpeakerCommand/VisionSpeakerConstants.h
+++ src/main/Commands/VisionSpeakerCommand/VisionSpeakerConstants.h
@@ -13,16 +13,18 @@ namespace VisionSpeakerConstants {
 
   static const InterpolatingTable<units::meter_t, units::degree_t> DistanceToLowerAngle{
       {
-        {0.1_m, -30_deg},
-        {0.5_m, -25_deg},
-        {1.0_m, -20_deg},
-        {1.5_m, -15_deg},
-        {2.0_m, -10_deg},
-        {2.5_m, -5_deg},
-        {3.0_m, 0_deg},
-        {3.5_m, 5_deg},
-        {4.0_m, 10_deg},
-        {4.5_m, 15_deg},
+        {1.66_m, -12.0_deg},
+        {1.9_m, -12.0_deg},
+        {2.4_m, -12.0_deg},
+        {2.9_m, -12.0_deg},
+        {3.4_m, -12.0_deg},
+        {3.9_m, -12.0_deg},
+        {4.4_m, -13.0_deg},
+        {4.9_m, -13.0_deg},
+        {5.4_m, -13.0_deg},
+        {5.9_m, -13.0_deg},
+        {6.4_m, -13.0_deg},
+        {6.9_m, -13.5_deg}
 
       }
 
@@ -30,16 +32,18 @@ namespace VisionSpeakerConstants {
 
   static const InterpolatingTable<units::meter_t, units::degree_t> DistanceToUpperAngle{
     {   
-        {0.1_m, 90_deg},
-        {0.5_m, 85_deg},
-        {1.0_m, 80_deg},
-        {1.5_m, 75_deg},
-        {2.0_m, 70_deg},
-        {2.5_m, 65_deg},
-        {3.0_m, 60_deg},
-        {3.5_m, 55_deg},
-        {4.0_m, 50_deg},
-        {4.5_m, 45_deg},
+        {1.66_m, 63.0_deg},
+        {1.9_m, 67.0_deg},
+        {2.4_m, 72.0_deg},
+        {2.9_m, 75.0_deg},
+        {3.4_m, 79.0_deg},
+        {3.9_m, 82.0_deg},
+        {4.4_m, 85.3_deg},
+        {4.9_m, 86.2_deg},
+        {5.4_m, 87.5_deg},
+        {5.9_m, 88.0_deg},
+        {6.4_m, 88.2_deg},
+        {6.9_m, 88.7_deg},
       
       }
 
