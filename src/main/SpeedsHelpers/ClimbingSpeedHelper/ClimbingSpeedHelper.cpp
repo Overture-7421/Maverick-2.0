aceade6d18a127bdf35c3be90e93db0b0f960994
Vanessa_Carranza <v.carranza.carri@gmail.com>
2024-10-14 15:50:11 -0600

    Climbing Speed Helper

--- src/main/SpeedsHelpers/ClimbingSpeedHelper/ClimbingSpeedHelper.cpp
+++ src/main/SpeedsHelpers/ClimbingSpeedHelper/ClimbingSpeedHelper.cpp
@@ -12,13 +12,15 @@ ClimbingSpeedHelper::ClimbingSpeedHelper(Chassis *chassis){
 	this->xPIDController.SetTolerance(0.05_m);
     this->headingPIDController.SetIZone(3);
 	this->headingPIDController.SetTolerance(0.05_deg);
+    headingPIDController.EnableContinuousInput(-180_deg, 180_deg);
+
 }
 
 void ClimbingSpeedHelper::alterSpeed(frc::ChassisSpeeds &inputSpeed){
 
-    units::meter_t xGoal = 2.5_m;
-    units::meter_t yGoal = 5_m;
-    units::degree_t rotationGoal = 90_deg;
+    units::meter_t xGoal = 1_m;
+    units::meter_t yGoal = 1_m;
+    units::degree_t rotationGoal = 75_deg;
 
     frc::Pose2d pose = chassis->getEstimatedPose();
 
@@ -26,7 +28,39 @@ void ClimbingSpeedHelper::alterSpeed(frc::ChassisSpeeds &inputSpeed){
     double yOut = yPIDController.Calculate(pose.Y(), yGoal);
     double rotationOut = headingPIDController.Calculate(pose.Rotation().Degrees(), rotationGoal);
 
+   
+    
+
+    if (headingPIDController.AtSetpoint()) {
+		rotationOut = 0;
+	}
+
+    if (xPIDController.AtSetpoint()) {
+        xOut = 0;
+    }
+
+    if (yPIDController.AtSetpoint()) {
+        yOut = 0;
+    }
+
+    inputSpeed.omega = units::degrees_per_second_t(rotationOut);
     inputSpeed.vx = units::meters_per_second_t(xOut);
-    inputSpeed.vy = units::meters_per_second_t(yOut);
-    inputSpeed.omega = units::radians_per_second_t(rotationOut);
+    inputSpeed.vy = units::meters_per_second_t(0);
+
+
+}
+
+void ClimbingSpeedHelper::getRotation(){
+    frc::Pose2d pose = chassis->getEstimatedPose();
+    frc::SmartDashboard::PutNumber("Rotation: ", pose.Rotation().Degrees().value());
+}
+
+void ClimbingSpeedHelper::getX(){
+    frc::Pose2d pose = chassis->getEstimatedPose();
+    frc::SmartDashboard::PutNumber("X: ", pose.X().value());
+}
+
+void ClimbingSpeedHelper::initialize(){
+    headingPIDController.Reset(chassis->getEstimatedPose().Rotation().Radians());
+
 }
--- src/main/SpeedsHelpers/ClimbingSpeedHelper/ClimbingSpeedHelper.h
+++ src/main/SpeedsHelpers/ClimbingSpeedHelper/ClimbingSpeedHelper.h
@@ -7,26 +7,30 @@
 #include <frc/controller/ProfiledPIDController.h>
 #include "Subsystems/Chassis/Chassis.h"
 #include <frc/geometry/Pose2d.h>
+#include <frc/smartdashboard/SmartDashboard.h>
 
 class ClimbingSpeedHelper : public SpeedsHelper{
  public:
   ClimbingSpeedHelper(Chassis *chassis);
   void alterSpeed(frc::ChassisSpeeds &inputSpeed) override;
+  void getRotation();
+  void getX();
 
+  void initialize() override;
 
 
  private:
   frc::ProfiledPIDController<units::meters> xPIDController{
 
-        0.0, 0.0, 0.0, {4.5_mps, 15_mps_sq} //Constraints max velocity, max acceleration
+        2.0, 0.0, 0.0, {4.5_mps, 2_mps_sq} //Constraints max velocity, max acceleration
     };
   frc::ProfiledPIDController<units::meters> yPIDController{
 
-        0.0, 0.0, 0.0, {4.5_mps, 15_mps_sq} //Constraints max velocity, max acceleration
+        2.0, 0.0, 0.0, {4.5_mps, 2_mps_sq} //Constraints max velocity, max acceleration
     };
   frc::ProfiledPIDController<units::degree> headingPIDController{
 
-        5, 0.0, 0.0, {1000_deg_per_s, 850_deg_per_s / 1_s} //Constraints max velocity, max acceleration
+        4, 0.0, 0.0, {500_deg_per_s, 1000_deg_per_s / 1_s} //Constraints max velocity, max acceleration
      };
 
 	Chassis *chassis;
